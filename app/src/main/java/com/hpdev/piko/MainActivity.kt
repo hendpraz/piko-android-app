0bf85bb74032bb45008b74c865fb88dfa4ed8f99
Hendry Prasetya <mhendryp99@gmail.com>
2021-07-13 15:40:39 +0700

    add edit text for searching contact

--- app/src/main/java/com/hpdev/piko/MainActivity.kt
+++ app/src/main/java/com/hpdev/piko/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hpdev.piko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
---
+++ app/src/main/java/com/hpdev/piko/ui/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.hpdev.piko.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hpdev.piko.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
