32f4794a8ca956033cc3990afcee337d83f558a4
Hendry Prasetya <mhendryp99@gmail.com>
2021-08-16 15:50:19 +0700

    add blank pages

---
+++ app/src/main/java/com/hpdev/piko/ui/favorites/FavoritesActivity.kt
@@ -0,0 +1,12 @@
+package com.hpdev.piko.ui.favorites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hpdev.piko.R
+
+class FavoritesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites)
+    }
+}
