57adb0d85f92654601580bd498eea79227fe22a9
Hendry Prasetya <mhendryp99@gmail.com>
2021-08-21 14:23:00 +0700

    update: leak canary

--- app/src/main/java/com/hpdev/piko/MainActivity.kt
+++ app/src/main/java/com/hpdev/piko/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.hpdev.piko
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
--- app/src/main/java/com/hpdev/piko/settings/SettingsActivity.kt
+++ app/src/main/java/com/hpdev/piko/settings/SettingsActivity.kt
@@ -1,12 +1,52 @@
 package com.hpdev.piko.settings
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.hpdev.piko.R
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var broadcastReceiver: BroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+    }
+
+    private fun registerBroadCastReceiver() {
+        val tvPowerStatus = findViewById<TextView>(R.id.tv_power_status)
+
+        broadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                when (intent.action) {
+                    Intent.ACTION_POWER_CONNECTED -> {
+                        tvPowerStatus.text = getString(R.string.power_connected)
+                    }
+                    Intent.ACTION_POWER_DISCONNECTED -> {
+                        tvPowerStatus.text = getString(R.string.power_disconnected)
+                    }
+                }
+            }
+        }
+        val intentFilter = IntentFilter()
+        intentFilter.apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+        }
+        registerReceiver(broadcastReceiver, intentFilter)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerBroadCastReceiver()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
     }
 }
